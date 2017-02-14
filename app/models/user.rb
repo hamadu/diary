2d9c5adf299da355cd4ed407feadf7dca91bf874
hamadu <dercll1020@gmail.com>
2017-02-15 01:05:28 +0900

    annotations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)      not null
+#  password        :string(255)      not null
+#  last_logined_at :datetime
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :name, presence: true
   validates :password, presence: true
